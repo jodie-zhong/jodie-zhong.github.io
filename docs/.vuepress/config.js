2c36546c15a41468ab516f1fdf8685b33eae759a
jodie <jodie-zhong@outlook.com>
2022-04-24 22:41:00 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Jodie',
+    description: 'Just playing around',
+    head: [
+        [ 'link', { rel: 'icon', href: '/jodie.jpg'}]
+    ],
+    themeConfig: {
+        logo: '/jodie.jpg',
+        darkMode: false
+    }
+}
